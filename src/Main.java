b007642e0963068bf22211c0f5954172b1e7dcb7
Danita <roboticmisfit@gmail.com>
2018-07-12 10:10:07 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    /*
+    Write an application that assigns 2 to a variable named mynumber and 1.7938 to a variable named myothernumber,
+    and then prints out the values to the console.
+    */
+
+        int mynumber;
+        double myothernumber;
+
+        mynumber = 2;
+        myothernumber = 1.7938;
+
+        System.out.println(mynumber);
+        System.out.println(myothernumber);
+
+    }
+
+
+}
